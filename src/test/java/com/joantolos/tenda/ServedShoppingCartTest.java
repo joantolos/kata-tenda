d70511acef2073d62ca8a0960a209023ff7e300f
joan <joan.tolos@gmail.com>
2023-04-02 19:15:48 +0200

    No need to store ShoppingCart on ComponentInShoppingCart

--- src/test/java/com/joantolos/tenda/ServedShoppingCartTest.java
+++ src/test/java/com/joantolos/tenda/ServedShoppingCartTest.java
@@ -14,9 +14,9 @@ public class ServedShoppingCartTest {
         Member memberClient = new Member("123", "John Doe", 13, 2023);
         ShoppingCart servedShoppingCart = new Served(new Date(), 15, memberClient);
 
-        ComponentInShoppingCart mouse = new ComponentInShoppingCart(servedShoppingCart, new Component("mouse", "This is a mouse", 50, 55), 2);
-        ComponentInShoppingCart keyboard = new ComponentInShoppingCart(servedShoppingCart, new Component("keyboard", "This is a regular USB keyboard", 50, 30), 1);
-        ComponentInShoppingCart display = new ComponentInShoppingCart(servedShoppingCart, new Component("display", "This is a big computer dispolay", 150, 20), 1);
+        ComponentInShoppingCart mouse = new ComponentInShoppingCart(new Component("mouse", "This is a mouse", 50, 55), 2);
+        ComponentInShoppingCart keyboard = new ComponentInShoppingCart(new Component("keyboard", "This is a regular USB keyboard", 50, 30), 1);
+        ComponentInShoppingCart display = new ComponentInShoppingCart(new Component("display", "This is a big computer dispolay", 150, 20), 1);
 
         servedShoppingCart.addComponent(mouse);
         servedShoppingCart.addComponent(keyboard);
